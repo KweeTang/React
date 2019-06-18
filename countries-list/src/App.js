3a1032c71049912952e6e5676134ae1b98ee89f7
Kwee T <kweetang@gmail.com>
2019-06-18 10:33:49 -0700

    Added search feature

--- countries-list/src/App.js
+++ countries-list/src/App.js
@@ -13,7 +13,8 @@ class App extends Component {
       isLoading: false,
       searchTerm: ''
     };
-
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -39,12 +40,36 @@ class App extends Component {
       });
     
   }
-  
+
+  handleChange(e) {
+      this.setState({searchTerm: e.target.value}); 
+  }
+
+  handleSubmit(e) {
+      e.preventDefault();
+      const url = `https://restcountries.eu/rest/v2/name/${this.state.searchTerm}`;
+
+      this.setState({isLoading: true, countries: []});
+      axios.get(url).then((response) => {
+          this.setState({countries: response.data, isLoading: false});
+      })
+      .catch((error) => {
+        this.setState({isLoading: false });
+      })
+  }
+
   render () {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <form onSubmit={this.handleSubmit}>
+          <input 
+            value = {this.state.searchTerm}
+            onChange = {this.handleChange}
+          />
+          <button type="Submit">Search</button>
+        </form>
         <ul> 
             {this.state.isLoading && 'Loading ...'}  
             {this.state.countries.map((country) => {
